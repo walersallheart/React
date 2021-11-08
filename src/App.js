1429f4419fccba8229c6d112d1561a0517a88f52
finalmegaman NA <finalmegaman@gmail.com>
2021-11-08 10:15:42 -0800

    40 The Concept of "Composition" ("children props")

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
-
+      <p>This is also visible!!</p>
+      
       <Expenses expenses={expenses}></Expenses>
     </div>
   );
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,11 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import Card from "./Card";
 
 function Expenses(props){
-    return (<div className="expenses">
-        {props.expenses.map((expense, i) => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}></ExpenseItem>)}
-    </div>);
+    return (<Card className="expenses">
+        {props.expenses.map((expense, i) => <ExpenseItem key={"expense-" + i} title={expense.title} amount={expense.amount} date={expense.date}></ExpenseItem>)}
+    </Card>);
 }
 
 export default Expenses;
