7e0d7fcba0084df5d74545a7b68d612a6c90e042
finalmegaman NA <finalmegaman@gmail.com>
2021-11-09 09:18:31 -0800

    53 Listening to User Input.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,24 +1,29 @@
-import "./ExpenseForm.css";
+import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const titleChangeHandler = event => {
+        console.log('Title changed');
+        console.log(event.target.value);
+    };
+
 	return (
 		<form>
-			<div className="new-expense__controls">
-				<div className="new-expense__control">
+			<div className='new-expense__controls'>
+				<div className='new-expense__control'>
 					<label> Title </label>
-                    <input type="text" />
+                    <input type='text' onChange={titleChangeHandler} />
 				</div>
-				<div className="new-expense__control">
+				<div className='new-expense__control'>
 					<label> Amount </label>
-                    <input type="number" min="0.01" step="0.01" />
+                    <input type='number' min='0.01' step='0.01' />
 				</div>
-				<div className="new-expense__control">
+				<div className='new-expense__control'>
 					<label> Date </label>
-					<input type="date" min="2019-01-01" max="2022-12-31" />
+					<input type='date' min='2019-01-01' max='2022-12-31' />
 				</div>
 			</div>
-            <div className="new-expense__actions">
-                <button type="submit">Add Expense</button>
+            <div className='new-expense__actions'>
+                <button type='submit'>Add Expense</button>
             </div>
 		</form>
 	);
