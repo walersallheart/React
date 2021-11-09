771da27f804ffef96549f9cbfd3b9c4b8703b696
finalmegaman NA <finalmegaman@gmail.com>
2021-11-09 12:11:17 -0800

    Assignment 4: Time to Practice Conditional Content.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import ExpenseForm from "./ExpenseForm";
 
 import './NewExpense.css';
 
 const NewExpense = props => {
+    const [isEditing, setIsEditing] = useState(false);
+
     const saveExpenseDataHandler = enteredExpenseData => {
         const expenseData = {
             ...enteredExpenseData,
@@ -11,11 +13,23 @@ const NewExpense = props => {
         }
 
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     }
 
-    return <div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
-    </div>
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    }
+
+    return (
+        <div className="new-expense">
+            { !isEditing && <button onClick={startEditingHandler}>Add New Expense</button> }
+            { isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} /> }
+        </div>
+    )
 }
 
 export default NewExpense;
