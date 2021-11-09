abc3b163f19df8c70af108debb3c18852b3355d9
finalmegaman NA <finalmegaman@gmail.com>
2021-11-09 12:02:54 -0800

    68 Adding Conditional Return Statements.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -11,14 +11,17 @@ const ExpenseItem = props => {
         setTitle('Updated');
     }
 
-    return (<Card className="expense-item">
-        <ExpenseDate date={props.date} />
-        <div className="expense-item__description">
-            <h2>{title}</h2>
-            <div className="expense-item__price">${props.amount}</div>
-        </div>
-        <button onClick={clickHandler}>Change Title</button>
-    </Card>);
+    return (
+    <li>
+        <Card className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+            <button onClick={clickHandler}>Change Title</button>
+        </Card>
+    </li>);
 }
 
 export default ExpenseItem;
