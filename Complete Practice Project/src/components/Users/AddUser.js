306a13873c18178e504dc992d4c42970c84e6fbe
finalmegaman NA <finalmegaman@gmail.com>
2021-11-12 11:32:44 -0800

    92 Adding a re-usable "Button" Component.

--- Complete Practice Project/src/components/Users/AddUser.js
+++ Complete Practice Project/src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 import classes from './AddUser.module.css'
 
@@ -19,7 +20,7 @@ const AddUser = props => {
                 <label htmlFor="age">Age (Years)</label>
                 <input type="number" name="age" id="age" />
 
-                <button type="submit">Add User</button>
+                <Button type="submit">Add User</Button>
             </form>
         </Card>
     );
