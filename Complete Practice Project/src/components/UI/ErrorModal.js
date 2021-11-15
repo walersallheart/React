474de0580462cac826218b7ee55c6ef866139a15
finalmegaman NA <finalmegaman@gmail.com>
2021-11-15 10:39:24 -0800

    98 Managing the Error State.

--- Complete Practice Project/src/components/UI/ErrorModal.js
+++ Complete Practice Project/src/components/UI/ErrorModal.js
@@ -8,7 +8,7 @@ import classes from './ErrorModal.module.css';
 const ErrorModal = props => {
     return (
         <div>
-            <div className={classes.backdrop}></div>
+            <div className={classes.backdrop} onClick={props.onConfirm}></div>
             <Card className={classes.modal}>
                 <header className={classes.header}>
                     <h2>{props.title}</h2>
@@ -17,7 +17,7 @@ const ErrorModal = props => {
                     <p>{props.message}</p>
                 </div>
                 <footer className={classes.actions}>
-                    <Button>Okay</Button>
+                    <Button onClick={props.onConfirm}>Okay</Button>
                 </footer>
             </Card>;
         </div>
