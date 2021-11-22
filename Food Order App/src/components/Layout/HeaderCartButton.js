3b53c75cab4b95cfb9b20e738f5aa5879529e523
walersallheart <53576683+walersallheart@users.noreply.github.com>
2021-11-22 11:56:19 -0800

    149 Using the useEffect Hook.

--- Food Order App/src/components/Layout/HeaderCartButton.js
+++ Food Order App/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
@@ -6,21 +6,44 @@ import CartContext from "../../store/cart-context";
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+    const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber += item.amount;
     }, 0);
 
-    return <button className={classes.button} onClick={props.onClick}>
-        <span className={classes.icon}>
-            <CartIcon />
-        </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>
-            { numberOfCartItems }
-        </span>
-    </button>;
+
+    const btnClasses = `${classes.button} ${buttonIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+
+        setButtonIsHighlighted(true);
+
+        const timer =  setTimeout(() => {
+            setButtonIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items]);
+
+    return (
+        <button className={btnClasses} onClick={props.onClick}>
+            <span className={classes.icon}>
+                <CartIcon />
+            </span>
+            <span>Your Cart</span>
+            <span className={classes.badge}>
+                { numberOfCartItems }
+            </span>
+        </button>
+    );
 }
 
 export default HeaderCartButton;
