597404393687bf3680cd62dc8b60aa471bfb28de
finalmegaman NA <finalmegaman@gmail.com>
2021-11-28 11:34:41 -0800

    177 using async/await.

--- Sending HTTP Requests/src/App.js
+++ Sending HTTP Requests/src/App.js
@@ -7,24 +7,19 @@ function App() {
   const [movies, setMovies] = useState([]);
 
   async function getMoviesHandler() {
-    await fetch('https://swapi.dev/api/films', {
+    const response = await fetch('https://swapi.dev/api/films', {});
+    const data = await response.json();
 
-    })
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        };
-      });
-      setMovies(transformedMovies);
-    })
-    .catch(err => console.log(err));
+    const transformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      };
+    });
+
+    setMovies(transformedMovies);
   }
 
   return (
