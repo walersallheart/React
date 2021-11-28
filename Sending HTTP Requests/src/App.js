671f33e8611673d79589886440591796de4f6b10
finalmegaman NA <finalmegaman@gmail.com>
2021-11-28 12:23:28 -0800

    182 Sending a POST Request.

--- Sending HTTP Requests/src/App.js
+++ Sending HTTP Requests/src/App.js
@@ -20,15 +20,18 @@ function App() {
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,8 +42,19 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
+  async function addMovieHandler(movie) {
     console.log(movie);
+
+    const response = await  fetch('https://react-http-65535-default-rtdb.firebaseio.com/movies.json', {
+      method:'POST',
+      body: JSON.stringify(movie),
+      header: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies.</p>;
