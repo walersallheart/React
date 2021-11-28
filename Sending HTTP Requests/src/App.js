a081b99da813f45e0b5c86a6f0127641ba5ed4cb
finalmegaman NA <finalmegaman@gmail.com>
2021-11-28 12:14:37 -0800

    181 Preparing The Project For the Next Steps.

--- Sending HTTP Requests/src/App.js
+++ Sending HTTP Requests/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-65535-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
