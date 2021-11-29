61eab4ec960a0872e508668cb2d3e30d52166173
finalmegaman NA <finalmegaman@gmail.com>
2021-11-29 12:59:59 -0800

    189 Configuring Custom Hooks.

--- Custom React Hooks/src/hooks/use-counter.js
+++ Custom React Hooks/src/hooks/use-counter.js
@@ -1,16 +1,20 @@
 import { useState, useEffect } from 'react';
 
 //custom hooks MUST start with the word "use"
-const useCounter = () => {
+const useCounter = (forwards = true) => {
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCounter((prevCounter) => prevCounter + 1);
+            if (forwards) {
+                setCounter((prevCounter) => prevCounter + 1);
+            } else {
+                setCounter((prevCounter) => prevCounter - 1);
+            }
         }, 1000);
 
         return () => clearInterval(interval);
-    }, []);
+    }, [forwards]);
 
     return counter;
 }
