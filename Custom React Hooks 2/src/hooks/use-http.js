0957228975f465d69960aaf2e9995c33dd79f90c
finalmegaman NA <finalmegaman@gmail.com>
2021-11-30 11:31:45 -0800

    192 Using the Custom Http Hook.

--- Custom React Hooks 2/src/hooks/use-http.js
+++ Custom React Hooks 2/src/hooks/use-http.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const useHttp = (requestConfig, applyData) => {
     const [isLoading, setIsLoading] = useState(false);
@@ -10,9 +10,9 @@ const useHttp = (requestConfig, applyData) => {
         try {
             const response = await fetch(
                 requestConfig.url, {
-                    method:requestConfig.method,
-                    headers: requestConfig.headers,
-                    body: JSON.stringify(requestConfig.body)
+                    method:requestConfig.method ? requestConfig.method : '',
+                    headers: requestConfig.headers ? requestConfig.headers : {},
+                    body: requestConfig.body ? JSON.stringify(requestConfig.body) : null
                 }
             );
 
