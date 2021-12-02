a5c86c0a75680958c8dfe7b98554fbcc497cc74e
Willy Alers <willy.alers@allheart.net>
2021-12-02 12:06:19 -0800

    221 Submitting & Sending Cart Data.

--- Food Order App Part 2/src/components/Cart/Checkout.js
+++ Food Order App Part 2/src/components/Cart/Checkout.js
@@ -48,7 +48,12 @@ const Checkout = props => {
             return;
         }
 
-        //submit for data
+        props.onConfirm({
+            name: enteredName,
+            street: enteredStreet,
+            city: enteredCity,
+            postalCode:enteredPostalCode
+        });
     };
 
     const nameControlClasses = `${classes.control} ${formInputsValidity.name ? '' : classes.invalid}`;
