16542052ae9cc8b6630200defd20e1a6ced4e2b8
Willy Alers <willy.alers@allheart.net>
2021-12-03 13:30:07 -0800

    252 Refresher / Practice: Part 2/2

--- Advanced Redux/src/components/Shop/Products.js
+++ Advanced Redux/src/components/Shop/Products.js
@@ -1,16 +1,53 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id:'p1',
+    price:6,
+    title:'My First Book',
+    description:'The first book I ever wrote'
+  },
+  {
+    id:'p2',
+    price:5,
+    title:'My Second Book',
+    description:'The second book I ever wrote'
+  },
+  {
+    id:'p3',
+    price:3,
+    title:'My Third Book',
+    description:'The third book I ever wrote'
+  },
+  {
+    id:'p4',
+    price:9,
+    title:'My Fourth Book',
+    description:'The fourth book I ever wrote'
+  },
+  {
+    id:'p5',
+    price:10,
+    title:'My Fifth Book',
+    description:'The fifth book I ever wrote'
+  }
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        { DUMMY_PRODUCTS.map(product => {
+          return <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        }) }
       </ul>
     </section>
   );
