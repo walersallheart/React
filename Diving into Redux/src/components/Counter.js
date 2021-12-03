9d2704d9235e92cdb015393fef1a52b4a268be2c
Willy Alers <willy.alers@allheart.net>
2021-12-03 09:47:04 -0800

    238 Working with Multiple State Properties.

--- Diving into Redux/src/components/Counter.js
+++ Diving into Redux/src/components/Counter.js
@@ -5,8 +5,11 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch =  useDispatch()
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type:'toggle' });
+  };
 
   const incrementHandler = () => {
     dispatch({ type:'increment' });
@@ -27,7 +30,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      { show && <div className={classes.value}>{counter}</div> }
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increment by 5</button>
--- Diving into Redux/src/store/index.js
+++ Diving into Redux/src/store/index.js
@@ -1,27 +1,40 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state =  { counter:0 }, action) => {
+const initialState  = { counter:0, showCounter:true };
+
+const counterReducer = (state =  initialState, action) => {
     if (action.type === 'increment') {
         return {
+            ...state,
             counter:state.counter + 1
         }
     }
 
     if (action.type === 'increase') {
         return {
+            ...state,
             counter:state.counter + action.amount
         }
     }
 
     if (action.type === 'decrement') {
         return {
+            ...state,
             counter:state.counter - 1
         }
     }
 
     if (action.type === 'decrease') {
         return {
+            ...state,
             counter:state.counter - action.amount
+        }
+    }
+
+    if (action.type === 'toggle') {
+        return {
+            ...state,
+            showCounter: !state.showCounter
         }
     }
 
