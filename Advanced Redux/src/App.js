4649b5257a270bec7ff6bd6967e7522a0d22114a
Willy Alers <willy.alers@allheart.net>
2021-12-03 19:21:10 -0800

    256 Using useEffect with Redux.

--- Advanced Redux/src/App.js
+++ Advanced Redux/src/App.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -6,6 +7,15 @@ import Products from './components/Shop/Products';
 
 function App() {
   const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  //useSelector causes this component to re-evaluate, and useEffect will run if the cart has changed
+  useEffect(() => {
+    fetch('https://react-http-65535-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    });
+  }, [cart]);
 
   return (
     <Layout>
