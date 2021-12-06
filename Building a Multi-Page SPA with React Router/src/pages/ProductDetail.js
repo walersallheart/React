131fc3a5cebcae2b94231abaf2fd96fd040a55e9
Willy Alers <willy.alers@allheart.net>
2021-12-06 09:17:18 -0800

    272 Extracting Route Params.

--- Building a Multi-Page SPA with React Router/src/pages/ProductDetail.js
+++ Building a Multi-Page SPA with React Router/src/pages/ProductDetail.js
@@ -1,6 +1,11 @@
+import { useParams } from "react-router";
+
 const ProductDetail = () => {
+    const params = useParams();
+    
     return <section>
         <h1>Product Detail</h1>
+        <p>{params.productId}</p>
     </section>
 }
 
