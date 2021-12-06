c423ed6bd6fce7899cd89fced4a103fe22595dd3
Willy Alers <willy.alers@allheart.net>
2021-12-06 10:37:24 -0800

    280 Adding Dummy Data & More Content.

--- React Router (Practice Project)/src/pages/AllQuotes.js
+++ React Router (Practice Project)/src/pages/AllQuotes.js
@@ -1,5 +1,13 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'George Carlin', text: "I almost don't feel the way I do" },
+    { id: 'q2', author: 'George Carlin', text: "A house is just a place to keep your stuff while you go out and get more stuff." },
+    { id: 'q3', author: 'George Carlin', text: "Think of how stupid the average person is, and realize half of them are stupider than that."}
+];
+
 const AllQuotes = () => {
-    return <h1>All Quotes Page</h1>
+    return <QuoteList quotes={DUMMY_QUOTES} />
 }
 
 export default AllQuotes;
