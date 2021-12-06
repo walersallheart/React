a569b27ccaed2ffb257baea31defb5e1426134f7
Willy Alers <willy.alers@allheart.net>
2021-12-06 10:18:30 -0800

    278 Practicing Nested Prefabs.

--- React Router (Practice Project)/src/pages/QuoteDetail.js
+++ React Router (Practice Project)/src/pages/QuoteDetail.js
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
-import { useParams } from "react-router";
+import { useParams, Route } from "react-router";
+
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
     const params = useParams();
@@ -7,6 +9,9 @@ const QuoteDetail = () => {
     return <Fragment>
         <h1>Quote Detail Page</h1>
         <p>{params.quoteId}</p>
+        <Route path={`/quotes/${params.quoteId}/comments`}>
+            <Comments />
+        </Route>
     </Fragment>;
 }
 
