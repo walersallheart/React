2da532f61c4ff0a2b82fa7bb52b3ec3f77f8223b
Willy Alers <willy.alers@allheart.net>
2021-12-07 17:02:28 -0800

    287 Writing More Flexible Routing Code.

--- React Router (Practice Project)/src/pages/QuoteDetail.js
+++ React Router (Practice Project)/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Route, Link } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
@@ -12,6 +12,7 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+    const match = useRouteMatch();
     const params = useParams();
 
     const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
@@ -22,12 +23,12 @@ const QuoteDetail = () => {
 
     return <Fragment>
         <HighlightedQuote text={quote.text} author={quote.author} />
-        <Route path={`/quotes/${params.quoteId}`}  exact>
+        <Route path={match.path}  exact>
             <div className='centered'>
-                <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>Load Comments</Link>
+                <Link className='btn--flat' to={`${match.url}/comments`}>Load Comments</Link>
             </div>
         </Route>
-        <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Route path={`${match.path}/comments`}>
             <Comments />
         </Route>
     </Fragment>;
