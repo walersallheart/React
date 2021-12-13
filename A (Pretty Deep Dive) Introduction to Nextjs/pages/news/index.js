7ca82b018a112a9e1ba5cd98340eb25134b78864
Willy Alers <willy.alers@allheart.net>
2021-12-12 19:53:19 -0800

    323 Adding Nested Paths & Pages (Nested Routes).

--- A (Pretty Deep Dive) Introduction to Nextjs/pages/news/index.js
+++ A (Pretty Deep Dive) Introduction to Nextjs/pages/news/index.js
@@ -1,7 +1,7 @@
 //our-domain.com/news
 
-const HomePage = () => {
+const NewsPage = () => {
     return <h1>The News Page</h1>
 }
 
-export default HomePage;
+export default NewsPage;
---
+++ A (Pretty Deep Dive) Introduction to Nextjs/pages/news/something-important.js
@@ -0,0 +1,7 @@
+ //our-domain.com/news/something-important
+
+const SomethingImportant = () => {
+    return <h1>Something Important</h1>
+}
+
+export default SomethingImportant;
