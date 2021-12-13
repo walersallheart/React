a70678d9993a20c4c472dd4fbc0bb305444b87e9
Willy Alers <willy.alers@allheart.net>
2021-12-12 20:16:40 -0800

    326 Linking Between Pages.

--- A (Pretty Deep Dive) Introduction to Nextjs/pages/news/index.js
+++ A (Pretty Deep Dive) Introduction to Nextjs/pages/news/index.js
@@ -1,7 +1,20 @@
 //our-domain.com/news
 
+import Link from 'next/link';
+import { Fragment } from "react";
+
 const NewsPage = () => {
-    return <h1>The News Page</h1>
+    return <Fragment>
+        <h1>The News Page</h1>
+        <ul>
+            <li>
+                <Link href="/news/nextjs-is-a-great-framework">
+                    NextJS is a Great Framework
+                </Link>
+            </li>
+            <li>Something Else</li>
+        </ul>
+    </Fragment>
 }
 
 export default NewsPage;
