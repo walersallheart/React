c72fef0111678d50ea8d41d89f7efbd1187802b5
Willy Alers <willy.alers@allheart.net>
2021-12-13 18:01:01 -0800

    329 Outputting a List of Meetups.

--- Onwards to a biggeer Project/pages/index.js
+++ Onwards to a biggeer Project/pages/index.js
@@ -1,5 +1,38 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Some address 512345 Some City',
+        description: 'This is a first meetup'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Some address 512345 Some City',
+        description: 'This is a second meetup'
+    },
+    {
+        id: 'm3',
+        title: 'A Third Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Some address 512345 Some City',
+        description: 'This is a third meetup'
+    },
+    {
+        id: 'm4',
+        title: 'A Fourth Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Some address 512345 Some City',
+        description: 'This is a fourth meetup'
+    }
+];
+
 const HomePage = () => {
-    return <h1></h1>;
+    return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
