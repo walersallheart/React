242e7eced4ca8f7c054bf73c5b9fdf998c0caf44
Willy Alers <willy.alers@allheart.net>
2021-12-14 09:37:16 -0800

    333 Adding Custom Components & CSS Modules.

---
+++ Onwards to a biggeer Project/pages/[meetupId]/index.js
@@ -0,0 +1,11 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+    return <MeetupDetail
+                image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
+                title='A First Meetup'
+                address='123 Fake Street'
+                description='The meetup description' />
+}
+
+export default MeetupDetails;
