8b319e0997b5dd8f53fe80e70b71d63c0989eac0
Willy Alers <willy.alers@allheart.net>
2021-12-14 12:36:29 -0800

    337 Exploring Server-side rendering (SSR) with "getServerSideProps".

--- Onwards to a biggeer Project/pages/[meetupId]/index.js
+++ Onwards to a biggeer Project/pages/[meetupId]/index.js
@@ -1,11 +1,30 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+const MeetupDetails = props => {
     return <MeetupDetail
-                image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
-                title='A First Meetup'
-                address='123 Fake Street'
-                description='The meetup description' />
+                id={props.id}
+                image={props.image}
+                title={props.title}
+                address={props.address}
+                description={props.description}/>
+}
+
+export function getStaticProps(context) {
+    const meetupId = context.params.meetupId;
+
+    console.log(meetupId);
+
+    return {
+        props: {
+            meetupData: {
+                id: meetupId,
+                image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+                title:'A First Meetup',
+                address:'123 Fake Street',
+                description:'The meetup description'
+            }
+        }
+    }
 }
 
 export default MeetupDetails;
