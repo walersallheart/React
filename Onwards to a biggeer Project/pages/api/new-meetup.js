3553f02fcad9278c7a61a68efe9f1d63fab82a66
Willy Alers <willy.alers@allheart.net>
2021-12-14 15:38:13 -0800

    342 Sending HTTP Requests To our api routes.

--- Onwards to a biggeer Project/pages/api/new-meetup.js
+++ Onwards to a biggeer Project/pages/api/new-meetup.js
@@ -1,10 +1,20 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
 
-        const { title, image, address, description } = data;
+        const client = await MongoClient.connect('mongodb+srv://willy:G33Kr0uJhV7xYb5T@cluster0.7uqja.mongodb.net/meetups?retryWrites=true&w=majority');
+        const db = client.db();
 
-        
+        const meetupsCollection = db.collection('meetups');
+
+        const result = await meetupsCollection.insertOne(data);
+        console.log(result);
+
+        client.close();
+
+        res.status(201).json({ message: 'Meetupd inserted!' })
     }
 }
 
