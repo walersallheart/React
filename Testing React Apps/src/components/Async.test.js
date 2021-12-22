30750a797aa53322370b1142b60a8616e62a7d22
Willy Alers <willy.alers@allheart.net>
2021-12-21 19:04:41 -0800

    392 Working WIth Mocks.

--- Testing React Apps/src/components/Async.test.js
+++ Testing React Apps/src/components/Async.test.js
@@ -4,6 +4,11 @@ import Async from './Async';
 
 describe('Async component', () => {
     test('renders posts if request succeeds', async ()=>{
+        window.fetch = jest.fn();
+        //this will replace the value of fetch the first time its called
+        window.fetch.mockResolvedValueOnce({
+            json:async () => [{id:'p1', title: 'First Post'}]
+        });
         render(<Async/>);
 
         //findAllByRole is a promise, while getAllByRole is synchronous
