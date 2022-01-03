07f7764f4ef37dbf117b0dbf6867f99052b30785
Willy Alers <willy.alers@allheart.net>
2022-01-03 09:47:31 -0800

    408 Working with Props & TypeScript.

--- React + Typescript/src/App.tsx
+++ React + Typescript/src/App.tsx
@@ -3,7 +3,7 @@ import Todos from './components/Todos';
 function App() {
   return (
     <div className="App">
-      <Todos/>
+      <Todos items={['Learn React!', 'Learn TypeScript!!']} />
     </div>
   );
 }
